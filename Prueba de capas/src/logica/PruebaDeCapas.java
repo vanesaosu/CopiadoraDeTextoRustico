79fb180358c005d519ab613db218e29f68a652fe
Mona Lisa <vanesa10osuna@gmail.com>
2023-07-24 10:40:24 -0300

    Agrego primeros archivos

---
+++ Prueba de capas/src/logica/PruebaDeCapas.java
@@ -0,0 +1,15 @@
+
+package logica;
+
+import igu.Pantalla;
+
+
+public class PruebaDeCapas {
+
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelative(null);
+    }
+    
+}
